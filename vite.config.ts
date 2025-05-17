f4fa41abb927a9a70f25a508e327045b7fcb4194
Paul Toner <ptoner92@gmail.com>
2025-05-17 19:15:34 -0400

    Build target: 'esnext' to allow module federation

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext', // This enables top-level await support
+    minify: 'terser',
+    cssCodeSplit: false
+  },
   plugins: [
     react(),
     federation({
