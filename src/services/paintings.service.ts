bc16dd98f94c35664e8b8c0fc2a827611c1bdebc
Paul Toner <ptoner92@gmail.com>
2025-09-25 13:40:08 -0400

    Amplify Gen 2 refactor for paintings.service

--- src/services/paintings.service.ts
+++ src/services/paintings.service.ts
@@ -1,30 +1,83 @@
-/**
- * Loads all available paintings from S3 bucket
- */
-export const loadPaintings = async () => {
-    try {
-        const paintings = await fetch('/api/paintings');
-        return paintings.json();
-    }
+import { getUrl, list, getProperties } from 'aws-amplify/storage';
+const painting1 = import.meta.env.VITE_PAULS_FAV_Painting1;
+const painting2 = import.meta.env.VITE_PAULS_FAV_Painting2;
+const painting3 = import.meta.env.VITE_PAULS_FAV_Painting3;
+import { GalleryImage } from '@/components/Paintings/PaintingsPage/ImageGallery';
 
-    catch (error) {
-        console.error('Error fetching paintings:', error);
+var paintings: GalleryImage[] = [];
+
+export const loadPaintingPreviews = async (): Promise<GalleryImage[]> => {
+  try {
+    const photoKeys = [painting1, painting2, painting3];
+
+    const photos = await Promise.all(
+      photoKeys.map(async (key) => {
+        const fullPath = `public/${key}`; // Construct the full path
+
+        // Fetch the image URL and metadata in parallel
+        const [urlResult, propertiesResult] = await Promise.all([
+          getUrl({ path: fullPath }),
+          getProperties({ path: fullPath }),
+        ]);
+
+        const imageUrl = urlResult.url;
+        const metaData = propertiesResult.metadata;
+
         return {
-            success: false,
-            message: 'There was a problem processing your request. Please try again later.'
+          key: key as string, // Original key
+          url: imageUrl.href,
+          title: metaData?.title,
+          createdDate: metaData?.createddate,
+          description: metaData?.description,
         };
-    }
+      })
+    );
+
+    return photos;
+  } catch (error) {
+    console.error("Error loading painting previews:", error);
+    // Handle the error appropriately, e.g., return an empty array or rethrow
+    return [];
+  }
 };
 
-export const loadPaintingPreviews = async () => {
+export const loadPaintings = async (): Promise<GalleryImage[]> => {
   try {
-    const response = await fetch(`/api/paintings/preview`);
-    return await response.json();
+    const { items } = await list({ path: 'public/paintings_webp/' });
+    // Filter out the "folder" entry with size 0
+    const imageUrls = items.filter(item => item.size != 0);
+
+    // If we already have all the paintings, return them
+    if (paintings.length == imageUrls.length) {
+      return paintings;
+    }
+    
+
+    // Concurrently fetch the URL and metadata for each image
+    const photos = await Promise.all(
+      imageUrls.map(async (item) => {
+        const [urlResult, propertiesResult] = await Promise.all([
+          getUrl({ path: item.path }),
+          getProperties({ path: item.path }),
+        ]);
+
+        const imageUrl = urlResult.url;
+        const metadata = propertiesResult.metadata;
+
+        return {
+          key: item.path,
+          url: imageUrl.href,
+          title: metadata?.title,
+          createdDate: metadata?.createddate,
+          description: metadata?.description,
+        };
+      })
+    );
+
+    paintings = photos;
+    return photos;
   } catch (error) {
-    console.error('Error fetching preview paintings:', error);
-    return {
-      success: false,
-      message: 'There was a problem processing your request. Please try again later.'
-    };
+    console.error("Error loading all painting previews:", error);
+    return [];
   }
 };
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,5 +1,8 @@
 interface ImportMetaEnv {
-    readonly VITE_CONTACT_EMAIL_URL: string
+    readonly VITE_CONTACT_EMAIL_URL: string,
+    readonly VITE_PAULS_FAV_Painting1: string,
+    readonly VITE_PAULS_FAV_Painting2: string,
+    readonly VITE_PAULS_FAV_Painting3: string,
   }
   
   interface ImportMeta {
