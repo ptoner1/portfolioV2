5b5d31f3e95d62785ebffe5986c1189660e45d23
Paul Toner <ptoner92@gmail.com>
2026-05-24 13:45:25 -0400

    initFederation() bugfix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/inter/300.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/700.css'
 import App from './App';
 import './index.css';
+import outputs from '../amplify_outputs.json';
+import { Amplify } from 'aws-amplify'
 import { initFederation } from '@angular-architects/native-federation-runtime';
 
+Amplify.configure(outputs);
 
 (async () => {
   try {
-    // Register the Native Federation application before mounting the DOM
-    await initFederation({
-      'naasApp': import.meta.env.VITE_NAAS_APP_URL
-    });
+    const naasUrl = import.meta.env.VITE_NAAS_APP_URL;
+    await initFederation(naasUrl);
     console.log('Production Native Federation initialized successfully.');
   } catch (err) {
     console.error('Failed to pre-load Native Federation manifest endpoints:', err);
